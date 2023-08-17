ca83d48eee7ac9f16f1b7211b701ccec604e3d01
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-08-17 16:14:47 -0500

    semicolons for the win

--- kill_views.js
+++ kill_views.js
@@ -2,8 +2,8 @@ function kill_views() {
 // not sure if this is the best way to go about it...
 let views = document.querySelectorAll('[href*="analytics"]');
 for (let i = 0; i < views.length; i++) {
-    views[i].remove()
-    console.log('removed: ' + views[i])
+    views[i].remove();
+    console.log('removed: ' + views[i]);
   } 
 }
 
